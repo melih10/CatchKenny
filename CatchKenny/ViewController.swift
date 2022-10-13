5f606ee65416ec73e8ff8affc74f46e7ad2b5574
melih10 <94173500+melih10@users.noreply.github.com>
2022-10-13 12:36:48 +0300

    Initial Commit

---
+++ CatchKenny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchKenny
+//
+//  Created by Melih on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
